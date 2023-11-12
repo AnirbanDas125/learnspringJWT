32617df4a8ebeca79856e1127281c1006e82d7ec
Anirban <ad653157@gmail.com>
2023-11-12 11:35:58 +0530

    Empty commit

---
+++ src/main/java/com/anirban/learnspringJWT/LearnspringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.anirban.learnspringJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringJwtApplication.class, args);
+	}
+
+}
