06674412a22578c85d9d4d922a7af1f9eec7e990
Anirban <ad653157@gmail.com>
2023-11-14 13:08:00 +0530

    added nothing new

--- src/main/java/com/anirban/learnspringJWT/controllers/HomeController.java
+++ src/main/java/com/anirban/learnspringJWT/controllers/HomeController.java
@@ -1,8 +1,10 @@
 package com.anirban.learnspringJWT.controllers;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,12 @@ public class HomeController {
 	@GetMapping("/users")
 	public List<Users> getUser() {
 		return homeService.getUsers();
+	}
+	
+	@GetMapping("/current-user")
+	public String getLoggedInUser(Principal principal) {
+/*In Java security, the Principal interface is a fundamental part of the Java Authentication and Authorization Service (JAAS) framework. 
+The Principal interface represents an entity (typically a user or a system) that is capable of being authenticated and authorized.*/		
+		return principal.getName();
 	}
 }
--- src/main/java/com/anirban/learnspringJWT/service/HomeService.java
+++ src/main/java/com/anirban/learnspringJWT/service/HomeService.java
@@ -20,6 +20,7 @@ public class HomeService {
 		return users;
 		
 	}
+
 	
 	
 
